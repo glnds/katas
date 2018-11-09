35f91b7160e37c7d6381e615e69f6cf717ce8fad
Gert Leenders <leenders.gert@gmail.com>
2018-11-09 09:03:04 +0100

    Pushing latest kata

--- src/bowling_game.py
+++ src/bowling_game.py
@@ -1,11 +1,97 @@
+
 class Game(object):
 
     def __init__(self):
+        super().__init__()
         self._score = 0
+        self._current_frame = Frame()
+        self._frames = [self._current_frame]
 
     def roll(self, fallen_pins):
-        self._score = fallen_pins
+        if self._current_frame.finished:
+            self._current_frame = Frame()
+            self._frames.append(self._current_frame)
+
+        self._current_frame.roll(fallen_pins)
 
     @property
     def score(self):
+
+        for index, frame in enumerate(self._frames):
+            print(frame)
+            if frame.is_strike:
+                self._score += 10
+                self._score += self._get_strike_bonus(index)
+                print('s' + str(self._score))
+            elif frame.is_spare:
+                self._score += 10
+                self._score += self._get_spare_bonus(index)
+                print('p' + str(self._score))
+            else:
+                print(frame.sum)
+                self._score += frame.sum
+                print('n' + str(self._score))
+
         return self._score
+
+    def _get_strike_bonus(self, index):
+        bonus = 0
+        if index+1 < len(self._frames):
+            bonus += self._frames[index+1].sum
+            if (self._frames[index+1].is_strike
+                    and index+2 < len(self._frames)
+                    and self._frames[index+2].first_roll):
+                bonus += self._frames[index+2].first_roll
+        return bonus
+
+    def _get_spare_bonus(self, index):
+        bonus = 0
+        if index+1 < len(self._frames):
+            bonus += self._frames[index+1].first_roll
+        return bonus
+
+
+class Frame(object):
+
+    def __init__(self):
+        super().__init__()
+        self._finished = False
+        self._first_roll = None
+        self._second_roll = None
+
+    def roll(self, fallen_pins):
+        if self._first_roll is None:
+            self._first_roll = fallen_pins
+            if fallen_pins == 10:
+                self._finished = True
+        else:
+            self._second_roll = fallen_pins
+            self._finished = True
+
+    @property
+    def first_roll(self):
+        return self._first_roll
+
+    @property
+    def finished(self):
+        return self._finished
+
+    @property
+    def sum(self):
+        if self._first_roll is None:
+            return 0
+        score = self._first_roll
+        if self._second_roll:
+            score += self._second_roll
+        return score
+
+    @property
+    def is_strike(self):
+        return self._first_roll == 10
+
+    @property
+    def is_spare(self):
+        return not self.is_strike and self.sum == 10
+
+    def __str__(self):
+        return "[%s, %s]" % (self._first_roll, self._second_roll)
--- tests/test_bowling_game.py
+++ tests/test_bowling_game.py
@@ -7,9 +7,66 @@ class TestBowlingGame(unittest.TestCase):
     def setUp(self):
         self.game = Game()
 
-    def test_new_game(self):
-        assert self.game.score == 0
+    def throw_spare(self):
+        self.game.roll(5)
+        self.game.roll(5)
 
-    def test_normal_roll(self):
+    def throw_strike(self):
+        self.game.roll(10)
+        self.game.roll(None)
+
+    # def test_new_game(self):
+    #     assert self.game.score == 0
+
+    # def test_one_throw(self):
+    #     self.game.roll(4)
+
+    #     assert self.game.score == 4
+
+    # def test_one_turn(self):
+    #     self.game.roll(3)
+    #     self.game.roll(2)
+
+    #     assert self.game.score == 5
+
+    # def test_throw_one_spare(self):
+    #     self.throw_spare()
+    #     self.game.roll(5)
+    #     self.game.roll(4)
+
+    #     assert self.game.score == 24
+
+    # def test_throw_one_srike(self):
+    #     self.throw_strike()
+    #     self.game.roll(5)
+    #     self.game.roll(4)
+
+    #     assert self.game.score == 28
+
+    # def test_throw_two_spares(self):
+    #     self.throw_spare()
+    #     self.throw_spare()
+    #     self.game.roll(5)
+    #     self.game.roll(4)
+
+    #     assert self.game.score == 39
+
+    def test_throw_two_strikes(self):
+        self.throw_strike()
+        self.throw_strike()
+        self.game.roll(5)
         self.game.roll(4)
-        assert self.game.score == 4
+
+        assert self.game.score == 53
+
+    # def test_gutter_game(self):
+    #     for i in range(20):
+    #         self.game.roll(0)
+
+    #     assert self.game.score == 0
+
+    def test_perfect_game(self):
+        for i in range(10):
+            self.throw_strike()
+
+        assert self.game.score == 300
